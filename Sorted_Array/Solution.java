0269398cb8eadd7a0c6362753dea137ac5f96eb4
shivanidoli <cuty.shivani.doli@gmail.com>
2018-10-05 14:20:33 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- Sorted_Array/Solution.java
+++ Sorted_Array/Solution.java
@@ -1,80 +1,76 @@
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 import java.util.Scanner;
 
 public class Solution 
 {
-	public static void main(String args[]) throws Exception
-	{
-		int n,m,i,j,k;
-		int []a;
-		int []b;
-		int[] c;
-	    Scanner sc= new Scanner(System.in);
-	    Scanner sc1= new Scanner(System.in);
-	   // System.out.println("enter n array size");
-	    n=sc.nextInt();
-	    //System.out.println("enter m array size");
-	    m=sc1.nextInt();
-	    a=new int[n];
-	    b=new int[m];
-	    c=new int[m+n];
-	   // System.out.println("enter n array elements");
-	    for(i=0;i<n;i++)
+  public static void main(String[] args)throws Exception
+ {		   
+     int[] a=null;
+     int[] b=null;
+     int n,m;
+    // Scanner sc=new Scanner(System.in);
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     String s=br.readLine();
+     String r=br.readLine();
+     n=Integer.parseInt(s);
+     m=Integer.parseInt(r);
+     String w[]=null;
+     String x[]=null;
+     a=new int[n];
+     b=new int[m];
+     String p=br.readLine();
+     String q=br.readLine();
+     w=p.split("[,.]");
+     x=q.split("[,.]");
+     for(int i=0;i<n;i++)
+      {
+        a[i]=Integer.parseInt(w[i]); 
+      }
+     for(int j=0;j<m;j++)
+     {
+    	 b[j]=Integer.parseInt(x[j]);
+     }
+     int[] c;
+     int i=0,j=0,k=0;
+     c=new int[m+n];
+     while(i<a.length&&j<b.length)
+     {
+      if(a[i]>b[j])
+      {
+        c[k]=b[j];
+         k++;
+         j++;
+       }
+	   else
 	   {
-	       a[i]=sc.nextInt();
+		   c[k]=a[i];
+		   k++;
+		   i++;
 	   }
-	   Arrays.sort(a);
-		/*for(i=0;i<n;i++)
-		{
-		System.out.print(a[i]);
-		}
-		System.out.println("\n enter m array elements");*/
-		for(j=0;j<m;j++)
-		{
-			b[j]=sc1.nextInt();
-		}
-		Arrays.sort(b);
-	/*	for(j=0;j<m;j++)
-		{
-			System.out.print(b[j]+"\n");
-		}*/
-		
-		i=0;
-		j=0;
-		k=0;
-		while(i<a.length&&j<b.length)
-		{
-			if(a[i]>b[j])
-			{
-				c[k++]=b[j];
-				j++;
-			}
-			else
-			{
-				c[k++]=a[i];
-				i++;
-			}
-		}
-		while(i<a.length)
-		{
-			c[k++]=a[i++];
-		}
-		while(j<b.length)
-		{
-			c[k++]=b[j++];
-		}
-		sc.close();
-		sc1.close();
-		for(k=0;k<c.length;k++)
-		{
-			if(k<c.length-1)
-			{
-			 System.out.print(c[k]+",");
-			}
-			else
-			{
-				 System.out.print(c[k]);
-			}
-		}
-	}
+      }
+     while(i<a.length)
+     {
+    	 c[k]=a[i];
+    	 k++;
+    	 i++;
+     }
+     while(j<b.length)
+     {
+    	 c[k]=b[j];
+    	 k++;
+    	 j++;
+     }
+     for(k=0;k<c.length;k++)
+     {
+      if(k<c.length-1)
+      {
+        System.out.print(c[k]+",");
+       }
+      else
+      {    
+       System.out.print(c[k]);
+      }
+      } 
+   }
 }
