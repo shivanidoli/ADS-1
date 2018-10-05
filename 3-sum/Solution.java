7df3ad03d4e0ec42f3676e08844a4e5cfe049ff7
shivanidoli <cuty.shivani.doli@gmail.com>
2018-10-05 12:37:26 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ 3-sum/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution
+{
+public static void main(String[] args)
+{
+	int []a;
+	int n;
+    System.out.println("enter array size");
+    Scanner sc= new Scanner(System.in);
+    n=sc.nextInt();
+    a=new int[n];
+    System.out.println("enter array elements");
+    for(int i=0;i<a.length;i++)
+   {
+       a[i]=sc.nextInt();
+   }
+   Arrays.sort(a);
+for(int i=0;i<a.length;i++)
+{
+System.out.print(a[i]);
+}
+for(int i=0;i<a.length;i++)
+{
+ int j=i+1;
+ int k=a.length-1;
+while(j<k)
+{
+if((a[i]+a[j]+a[k])==0)
+{
+ System.out.println("true");
+  return;
+}
+else if((a[i]+a[j]+a[k])<0)
+{
+j++;
+}
+else if((a[i]+a[j]+a[k])>0)
+{
+k--;
+}
+
+sc.close();
+}
+
+}
+System.out.println("false");
+}
+}
