eeaafc877c87e6b61a5d44dbf460533e4f56f563
shivanidoli <cuty.shivani.doli@gmail.com>
2018-10-06 12:07:07 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- 4.2 Evaluate Postfix/Solution.java
+++ 4.2 Evaluate Postfix/Solution.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class Solution
 {
 	public static void main(String[] args)
 	{
+                Scanner sc=new Scanner(System.in);
 		String s;
+                s=sc.nextLine();
 		Stack st=new Stack();
+                st.postfix(s);
 	}
 
 }
