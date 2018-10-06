ad534a805795096aa292baf4f56b569ba1e3d5fb
shivanidoli <cuty.shivani.doli@gmail.com>
2018-10-06 11:04:22 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ 4.2 Evaluate Postfix/Solution.java
@@ -0,0 +1,104 @@
+public class Solution
+{
+	public static void main(String[] args)
+	{
+		String s;
+		Stack st=new Stack();
+	}
+
+}
+class Node
+{
+int data;
+Node next;
+
+Node(int data)
+{
+	this.data=data;
+	this.next=null;
+}
+}
+class Stack
+{
+	Sort so=new Sort();
+	int postfix(String s)
+	{
+		int i;
+		for(i=0;i<s.length();i++)
+		{
+			char c=s.charAt(i);
+			if(c==' ')
+			{
+				continue;
+			}
+			else if(Character.isDigit(c))
+			{
+				int n=0;
+				while(Character.isDigit(c))
+				{
+					n=n*10+(int)(c-'0');
+					i++;
+					c=s.charAt(i);
+				}
+				i--;
+				so.push(n);
+				}
+			else
+			{
+				int result=0;
+				int c1=so.pop();
+				int c2=so.pop();
+				switch(c)
+				{
+				case '+':
+					result=c1+c2;
+					so.push(result);
+					break;
+				case '-':
+					result=c1-c2;
+					so.push(result);
+					break;
+				case '*':
+					result=c1*c2;
+					so.push(result);
+					break;
+				case '/':
+					result=c1/c2;
+					so.push(result);
+					break;
+			    }
+				
+				}
+			}
+		
+		return so.pop();
+		}
+}
+class Sort
+{
+	Node head=null;
+	boolean isEmpty()
+	{
+		return head==null;
+	}
+	public void push(int data)
+	{
+		Node n=new Node(data);
+		if(head==null)
+		{
+			head=n;
+		}
+		else
+		{
+			n.next=head;
+			head=n;
+		}
+	}
+	int pop()
+	{
+		Node temp=head;
+		head=head.next;
+		System.out.println(temp.data);
+		return 0;
+	}
+}
